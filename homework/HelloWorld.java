90c8d9c5dd3004a832600f94cee95f272a569d59
milan <milan4202@gmail.com>
2025-07-27 18:30:27 +0530

    Create HelloWorld.java

---
+++ homework/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.homework.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Print my name 
+				System.out.println("My name is: Milan");	
+			
+				//Print my City Name 
+				System.out.println("My city name is: Somnath" );	
+			
+	}
+
+}
